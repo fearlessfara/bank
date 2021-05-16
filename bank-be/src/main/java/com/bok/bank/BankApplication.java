9b89e506c4291a774cb741846938bdf670f6dc24
Domenico Fasano <dfasano@soldo.com>
2021-05-16 23:43:48 +0200

    added eureka annotation

--- bank-be/src/main/java/com/bok/bank/BankApplication.java
+++ bank-be/src/main/java/com/bok/bank/BankApplication.java
@@ -3,11 +3,13 @@ package com.bok.bank;
 import com.bok.bank.setup.BankDatabaseSetup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Import;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient
 @Import(BankDatabaseSetup.class)
 public class BankApplication {
 
