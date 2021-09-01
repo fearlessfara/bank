803568454aa538e69644dd159436af2bb32d97f2
Domenico Fasano <dfasano@soldo.com>
2021-09-01 18:57:37 +0200

    fixed a bug

--- bank-be/src/main/java/com/bok/bank/messaging/AccountConsumer.java
+++ bank-be/src/main/java/com/bok/bank/messaging/AccountConsumer.java
@@ -3,6 +3,8 @@ package com.bok.bank.messaging;
 import com.bok.bank.exception.AccountException;
 import com.bok.bank.exception.BankAccountException;
 import com.bok.bank.helper.AccountHelper;
+import com.bok.bank.integration.dto.WireTransferRequestDTO;
+import com.bok.bank.integration.service.TransactionController;
 import com.bok.bank.model.Account;
 import com.bok.bank.model.BankAccount;
 import com.bok.bank.repository.AccountRepository;
@@ -19,6 +21,7 @@ import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
+import java.time.LocalDate;
 
 @Component
 @Slf4j
@@ -36,6 +39,8 @@ public class AccountConsumer {
     ConfirmationEmailHistoryRepository confirmationEmailHistoryRepository;
     @Autowired
     TransactionRepository transactionRepository;
+    @Autowired
+    TransactionController transactionController;
 
     @JmsListener(destination = "${active-mq.bank-account-deletion}")
     @Transactional
@@ -44,7 +49,9 @@ public class AccountConsumer {
         Account account = accountRepository.findById(message.accountId).orElseThrow(AccountException::new);
         BankAccount bankAccount = bankAccountRepository.findByAccountId(account.getId()).orElseThrow(BankAccountException::new);
         if (bankAccount.getAvailableAmount().isGreaterThan(Money.ZERO) || bankAccount.getBlockedAmount().isGreaterThan(Money.ZERO)) {
-            log.info("The account {} cannot be delete because it contain an available amount: {} and blocked: {}", message.accountId, bankAccount.getAvailableAmount(), bankAccount.getBlockedAmount());
+            log.warn("The account {} contain an available amount: {} and blocked: {}", message.accountId, bankAccount.getAvailableAmount(), bankAccount.getBlockedAmount());
+            transactionController.wireTransfer(message.accountId, new WireTransferRequestDTO(message.iban, account.getName(), "Account Closure",
+                    new com.bok.bank.integration.util.Money(bankAccount.getAvailableAmount().getCurrency(), bankAccount.getAvailableAmount().getValue()), LocalDate.now(), true));
         }
         confirmationEmailHistoryRepository.deleteByAccount(account);
         transactionRepository.deleteByFromBankAccount(bankAccount);
